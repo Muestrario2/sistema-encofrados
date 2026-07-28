d21d6dbb8e656bcd9ed3af3981978a03ec878f01
Muestrario2 <86855774+Muestrario2@users.noreply.github.com>
2026-07-27 23:59:08 -0500

    Add files via upload

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Formato de conexión a PostgreSQL con tu contraseña real
-URL_BASE_DATOS = "postgresql://postgres:jeffry16022004@localhost:5432/inventario_encofrado"
+URL_BASE_DATOS = "postgresql://postgres:jeffry16022004@db.upvprwmuatopbrppclvb.supabase.co:5432/postgres"
 
 engine = create_engine(URL_BASE_DATOS)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
